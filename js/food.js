d5189ad28d61cbc25a841d8a7385ae37f90be9a2
jcy <jcy981215@naver.com>
2023-08-25 13:21:28 +0900

    food

--- js/food.js
+++ js/food.js
@@ -1,7 +1,7 @@
-const btn = document.querySelectorAll('main .drink_list div .drink_poji .poji_title > a')
-const txt = document.querySelectorAll('main .drink_list > div .drink_txt')
-const div = document.querySelectorAll('main .drink_list > div')
-const xicon = document.querySelectorAll('main .drink_list div .drink_txt .txt_title > a')
+const btn = document.querySelectorAll('main .food_list div .food_poji .poji_title > a')
+const txt = document.querySelectorAll('main .food_list > div .food_txt')
+const div = document.querySelectorAll('main .food_list > div')
+const xicon = document.querySelectorAll('main .food_list div .food_txt .txt_title > a')
 console.log(btn,txt,div,xicon)
 for(let i of txt){i.style.display = 'none'}
 btn.forEach(function(t,i,a){
