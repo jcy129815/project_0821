49f41c7b48b1473bb635d76ba9c4ab9b93d8e36b
jcy <jcy981215@naver.com>
2023-08-25 10:15:25 +0900

    drinkhtml

--- js/drink.js
+++ js/drink.js
@@ -3,13 +3,13 @@ const txt = document.querySelectorAll('main .drink_list > div .drink_txt')
 const div = document.querySelectorAll('main .drink_list > div')
 console.log(btn,txt,div)
 for(let i of txt){i.style.display = 'none'}
-div.forEach(function(t,i,a){
-    t.addEventListener('mouseover',function(e){
+btn.forEach(function(t,i,a){
+    t.addEventListener('click',function(e){
         e.preventDefault()
         console.log(txt)
         txt[i].style.display ='block'
-    })
+    })/* 
     div[i].addEventListener('mouseout',function(){
         txt[i].style.display = 'none'
-    })
+    }) */
 })
