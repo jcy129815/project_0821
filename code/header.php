fd8fd5f428fce541cf4c05e354c9492f9f08605c
jcy <jcy981215@naver.com>
2023-09-11 18:00:48 +0900

    코드수정

--- code/header.php
+++ code/header.php
@@ -1,5 +1,50 @@
 <header>
         <h1><a href="./index.html"><img src="../tastimg/header_logo.png" alt=""></a></h1>
+        <a id="on" href="#"><img src="../tastimg/nav_btn.png" alt=""></a>
+        <div class="phon">
+            <a id="off" href="#"><img src="../tastimg/X.png" alt=""></a>
+            <ul class="phon_nav">
+                <li><a href="#">메가스토리</a>
+                    <ul class="phon_sub_nav">
+                        <li><a href="#">브랜드</a></li>
+                        <li><a href="#">BI</a></li>
+                        <li><a href="./map.html">오시는길</a></li>
+                    </ul>
+                </li>
+                <li><a href="./menu.html">메뉴소개</a>
+                    <ul class="phon_sub_nav">
+                        <li><a href="./drink.html">음료</a></li>
+                        <li><a href="./food.html">푸드</a></li>
+                        <li><a href="./shap.html">상품</a></li>
+                    </ul>
+                </li>
+                <li><a href="./map.html">매장</a>
+                    <ul class="phon_sub_nav">
+                        <li><a href="./map.html">매장 찾기</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">창업안내</a>
+                    <ul class="phon_sub_nav">
+                        <li><a href="#">개설비용</a></li>
+                        <li><a href="#">인테리어</a></li>
+                        <li><a href="#">창업문의</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">메가소식</a>
+                    <ul class="phon_sub_nav">
+                        <li><a href="#">공지사항</a></li>
+                        <li><a href="#">이벤트</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="./review.html">고객의 소리</a></li>
+                    </ul>
+                </li> 
+                <li><a href="#">채용</a>
+                    <ul class="phon_sub_nav">
+                        <li><a href="#">채용안내</a></li>  
+                    </ul>
+                </li>
+            </ul>
+        </div>
         <nav>
             <ul class="main_nav">
                 <li><a href="#">메가스토리</a>
